d0cdd63ed543adfc2d497dd85d9242928d19d6b2
anelkasam <orofluido16>
2017-12-01 17:01:21 +0200

    Set up docker

--- easymath/urls.py
+++ easymath/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/', include('service.urls')),
+    url(r'^', include('service.urls')),
     url(r'^library/', include('library.urls')),
 ]
